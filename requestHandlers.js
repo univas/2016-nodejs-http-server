e6cdab61803b40a6cb45f800d8765a86c375d5fb
Edy Segura <edysegura@gmail.com>
2016-11-10 21:59:01 -0200

    End to end integration

--- requestHandlers.js
+++ requestHandlers.js
@@ -5,21 +5,33 @@ const queryString = require('querystring')
 
 function login(response) {
     console.log('Request handler "Login" was called')
+    
+    let fileName = "./index.html"
+    responseHTML(response, fileName)
+}
 
-    fs.readFile("./index.html", "utf-8", function onReturn(error, data) {
+function auth(response, postData) {
+    console.log('Request handler "Auth" was called')
+    
+    let formData = queryString.parse(postData)
+    let fileName = isValidUser(formData.email, formData.password) 
+                   ? "./success.html" : "./access-denied.html"
+
+    responseHTML(response, fileName)
+}
+
+function isValidUser(email, password) {
+    return (email === 'edysegura@gmail.com' && password === 'batatinha')
+}
+
+function responseHTML(response, fileName) {
+    fs.readFile(fileName, "utf-8", function onReturn(error, data) {
         if(error) throw error
         response.statusCode = 200
         response.setHeader("Content-Type", "text/html")
         response.write(data)
         response.end()
     })
-}
-
-function auth(response, postData) {
-    console.log('Request handler "Auth" was called')
-    let formData = queryString.parse(postData)
-    response.write("You have sent: " + JSON.stringify(formData))
-    response.end()
 }
 
 //Public API
